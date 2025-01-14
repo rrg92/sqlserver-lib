9dc3b83bcd5575c3cb6b5955ffa72ccfede9c12a
Rodrigo Ribeiro Gomes <rodrigor.gomes@hotmail.com>
2025-01-14 11:55:05 -0300

    Ajustes no script

--- Modulos/ProcuraEmTodosModulos.sql
+++ Modulos/ProcuraEmTodosModulos.sql
@@ -1,15 +1,30 @@
 /*
-	Procura um texto em todos os módulos de todos os bancos.	
+	# autor 
+		Rodrigo Ribeiro Gomes 
+		
+	# detalhes 
+		Esse script é um dos que mais usei e até hoje uso!
+		Ele permite procurar um texto em todos módulos de todos os banco e nos jobs.
+		Módulo é funcão, procedure ou trigger.
+		Ou seja, você consegue procurar, por exemplo, se uma proc referencia uma tabela, ou até uma string direto em um filtro...
+		
+		Obviamente, que ele não é perfeito (não pega os caso adhoc) e pode pegar coisa além do que você precisa!
+		Mas, já pode te ajudar demais se você procura por algo bem específico!
+		
+		Esse não é um script pensando em performance.
+		Se você tiver muito objeto (MUITO mesmo), ele vai ser lento msmo e pode consumir bastante e CPU (e até causar alguma pressão de memória e atividade no disco).
+		Mas, em casos emergenciais, dificilmente você estará preocupado com isso.
+		
 */
 
 DECLARE
 	 @procura nvarchar(max)
-SET  @procura = 'chamados' 
+SET  @procura = 'TRAINEE' 
 
 IF OBJECT_ID('tempdb..#resultados') IS NOT NULL
 	DROP TABLE #Resultados
 CREATE TABLE
-	#Resultados ( Banco sysname,NomeObjeto sysname  )
+	#Resultados ( Banco sysname,NomeObjeto nvarchar(2000)  )
 
 DECLARE
 	@SQL varchar(max)
@@ -22,11 +37,11 @@ SET @SQL = '
 		#Resultados( bANCO,NomeObjeto )
 	SELECT
 		 db_name() COLLATE DATABASE_DEFAULT
-		,object_name(  sm.object_id, db_id() ) COLLATE DATABASE_DEFAULT
+		,object_name(  sm.object_id, db_id() ) COLLATE SQL_Latin1_General_CP1_CI_AI 
 	FROM
 		sys.sql_modules sm
 	WHERE
-		sm.definition like ''%'+@procura+'%''
+		sm.definition like ''%'+@procura+'%'' COLLATE SQL_Latin1_General_CP1_CI_AI 
 
 	UNION
 
@@ -36,7 +51,7 @@ SET @SQL = '
 	FROM
 		sys.syscomments sm
 	WHERE
-		text like ''%'+@procura+'%''
+		text like ''%'+@procura+'%'' COLLATE SQL_Latin1_General_CP1_CI_AI 
 
 
 ';
@@ -53,17 +68,17 @@ set nocount on;
 INSERT INTO
 	#Resultados( bANCO,NomeObjeto )
 SELECT
-	'--JOB--' COLLATE DATABASE_DEFAULT
-	,J.name COLLATE DATABASE_DEFAULT
+	'--JOB--' COLLATE SQL_Latin1_General_CP1_CI_AI 
+	,J.name COLLATE SQL_Latin1_General_CP1_CI_AI 
 FROM
 	msdb..sysjobs J
 	JOIN
 	msdb..sysjobsteps JS
 		ON JS.job_id = J.job_id
 WHERE
-	JS.command like '%'+@procura+'%'
+	JS.command like '%'+@procura+'%' COLLATE SQL_Latin1_General_CP1_CI_AI
 	OR
-	J.name like  '%'+@procura+'%'
+	J.name like  '%'+@procura+'%' COLLATE SQL_Latin1_General_CP1_CI_AI
 
 
 select *From #Resultados
